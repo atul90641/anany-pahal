a0c592964fbaf4c05f076b301fc097445eb3914f
Atul Kumar <atul90641@gmail.com>
2024-08-10 19:38:24 +0530

    removed key and url from supabase client

--- client/src/Pages/supabaseClient.js
+++ client/src/Pages/supabaseClient.js
@@ -4,14 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Replace with your Supabase URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Replace with your public API key
 
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Key:', supabaseKey);
-
-// Ensure both variables are available before creating the client
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase URL or Key is missing');
-}
-
 // Create a Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
 
